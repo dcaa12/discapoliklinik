41f8204587a1feceea079633e05a32deb4c71d03
dcaa12 <ikhligad@gmail.com>
2022-12-12 08:59:12 +0700

    disca

---
+++ application/views/v_dashboard.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <div class="jumbotron">
+        <h1 class="display-4">KLINIK DISCA</h1>
+        <p class="lead">TEMPAT BEROBAT ANDALAN KELUARGA</p>
+        <hr class="my-4">
+    </div>
+    <br><br>
+    <!-- <img src="<?= base_url('assets/orang.gif') ?>" alt=""> -->
+</div>
